54e0ae24f4115540e0d21352ffdc3b1098d9f3d3
Simon Schwegler <simulo2000@gmail.com>
2016-05-29 22:51:50 +0200

    More work on coroutine implementation (WIP)

--- BoostCoroutinesLite/src/Main.cpp
+++ BoostCoroutinesLite/src/Main.cpp
@@ -1,38 +1,69 @@
 #include <iostream>
 #include <vector>
 
-#include <boost/coroutine/asymmetric_coroutine.hpp>
+#include <boost/coroutine/coroutine.hpp>
 using namespace boost::coroutines;
+
+pull_coroutine<void>* g_outerCoroutine;
 
 void StartCoroutine(void(*runner)(push_coroutine<void>&))
 {
+	g_outerCoroutine = new pull_coroutine<void>{ runner };
+}
+
+void StartInnerCoroutine(void(*runner)(push_coroutine<void>&), push_coroutine<void>& yield)
+{
 	pull_coroutine<void> gen{ runner };
+	yield();
 
 	while (gen)
 	{
 		gen();
+		yield();
 	}
 }
 
-void simpler(push_coroutine<void>& yield)
+void inner(push_coroutine<void>& yield)
 {
-	std::cout << "simpler 1" << std::endl;
-	yield();
+	int max = 3;
+	for (int i = 0; i < max; i++)
+	{
+		std::cout << "inner " << i << std::endl;
+		yield();
+	}
 
-	std::cout << "simpler 2" << std::endl;
+	std::cout << "inner " << max << std::endl;
 }
 
-void simple(push_coroutine<void>& yield)
+void outer(push_coroutine<void>& yield)
 {
-	std::cout << "simple 1" << std::endl;
+	std::cout << "outer 1" << std::endl;
 	yield();
 
-	StartCoroutine(simpler);
+	StartInnerCoroutine(inner, yield);
 
-	std::cout << "simple 2" << std::endl;
+	std::cout << "outer 2" << std::endl;
+}
+
+void run()
+{
+	while (*g_outerCoroutine)
+	{
+		(*g_outerCoroutine)();
+		std::cout << "main" << std::endl;
+	}
+
+	delete g_outerCoroutine;
 }
 
 int main()
 {
-	StartCoroutine(simple);
+	StartCoroutine(outer);
+	std::cout << "main" << std::endl;
+
+	run();
+
+	std::cout << "done" << std::endl;
+
+	std::cin.get();
 }
