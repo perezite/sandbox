77409723bf42916231c513f4547d12cd889336ed
Simon Schwegler <simulo2000@gmail.com>
2016-09-27 00:53:09 +0200

    Updating folder structure

---
+++ NDKLoadLibrary/build/android/lib-build/OpenGLEngine/jni/whatever/local/armeabi-v7a/objs/OpenGLEngine/__/Engine/src/engine.o.d
@@ -0,0 +1,5 @@
+whatever/local/armeabi-v7a/objs/OpenGLEngine/__/Engine/src/engine.o: \
+ Z:/Indie/Development/Simulo/sandbox/NDKLoadLibrary/build/android/lib/OpenGLEngine/build/jni/../Engine/src/engine.cpp \
+ Z:/Indie/Development/Simulo/sandbox/NDKLoadLibrary/build/android/lib/OpenGLEngine/build/jni/../Engine/include/engine.h
+
+Z:/Indie/Development/Simulo/sandbox/NDKLoadLibrary/build/android/lib/OpenGLEngine/build/jni/../Engine/include/engine.h:
--- NDKLoadLibrary/build/android/ndk-tools/jni/src/Android.mk
+++ NDKLoadLibrary/build/android/ndk-tools/jni/src/Android.mk
@@ -4,8 +4,8 @@ LOCAL_PATH := $(call my-dir)
 ##################
 include $(CLEAR_VARS)
 LOCAL_MODULE := openGLSharedLibrary
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libOpenGLEngine.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/OpenGLEngine/$(TARGET_ARCH_ABI)/libOpenGLEngine.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/OpenGLEngine
 include $(PREBUILT_SHARED_LIBRARY)
 
 # Application Module
@@ -14,9 +14,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL2
+MY_CODE_PATH := $(LOCAL_PATH)/../../../../../src
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-    main.cpp
+    $(MY_CODE_PATH)/main.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2 openGLSharedLibrary
 
