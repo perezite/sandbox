3bc530bc241701efbc487b093252eb22b6d77d69
Simon Schwegler <simulo2000@gmail.com>
2016-10-13 17:48:27 +0200

    Added Application tier in build

--- SDL2MultiTierDebugging/build/android/ndk/jni/src/Android.mk
+++ SDL2MultiTierDebugging/build/android/ndk/jni/src/Android.mk
@@ -1,4 +1,6 @@
 LOCAL_PATH := $(call my-dir)
+SDL_PATH := ../SDL2
+MY_CODE_PATH := $(LOCAL_PATH)/../../../../../src
 
 # External module
 ##################
@@ -8,20 +10,21 @@ LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/OpenGLEngine/$(TARGET_ARCH_ABI)/li
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/OpenGLEngine
 include $(PREBUILT_SHARED_LIBRARY)
 
-# Application Module
+# Application module
+####################
+include $(CLEAR_VARS)
+LOCAL_MODULE := Application
+LOCAL_SRC_FILES := $(wildcard $(MY_CODE_PATH)/Application/*.cpp)
+LOCAL_EXPORT_C_INCLUDES := MY_CODE_PATH)/Application
+LOCAL_SHARED_LIBRARIES := SDL2 openGLSharedLibrary
+include $(BUILD_STATIC_LIBRARY)
+
+# Main module
 #####################
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
-
-SDL_PATH := ../SDL2
-MY_CODE_PATH := $(LOCAL_PATH)/../../../../../src
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-    $(MY_CODE_PATH)/main.cpp \
-    $(wildcard $(MY_CODE_PATH)/Application/*.cpp)
-    
-LOCAL_SHARED_LIBRARIES := SDL2 openGLSharedLibrary
-
+    $(MY_CODE_PATH)/main.cpp  
+LOCAL_STATIC_LIBRARIES := Application openGLSharedLibrary
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
-
 include $(BUILD_SHARED_LIBRARY)
