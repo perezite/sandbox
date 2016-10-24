328dac988692054c080b5398d98a6fa1cda02ce9
Simon Schwegler <simulo2000@gmail.com>
2016-10-24 13:54:40 +0200

    Finished decoupling

--- SDL2AndroidDecoupled/build/android/ndk/jni/src/Android.mk
+++ SDL2AndroidDecoupled/build/android/ndk/jni/src/Android.mk
@@ -2,25 +2,22 @@ LOCAL_PATH := $(call my-dir)
 
 # External module
 ##################
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := openGLSharedLibrary
-# LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/OpenGLEngine/$(TARGET_ARCH_ABI)/libOpenGLEngine.so
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/OpenGLEngine
-# include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE := SDL2
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/SDL2/$(TARGET_ARCH_ABI)/libSDL2.so
+# LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include/SDL2
+# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/SDL2
+include $(PREBUILT_SHARED_LIBRARY)
 
 # Application Module
 #####################
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
-
-SDL_PATH := ../SDL2
+#SDL_PATH := ../SDL2
 MY_CODE_PATH := $(LOCAL_PATH)/../../../../../src
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include
+LOCAL_SRC_FILES := SDL_android_main.c \
     $(MY_CODE_PATH)/main.cpp
-
 LOCAL_SHARED_LIBRARIES := SDL2
-
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
-
 include $(BUILD_SHARED_LIBRARY)
--- SDL2AndroidDecoupled/src/main.cpp
+++ SDL2AndroidDecoupled/src/main.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <string>
 
-#include "SDL.h"
+#include "SDL2/SDL.h"
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
