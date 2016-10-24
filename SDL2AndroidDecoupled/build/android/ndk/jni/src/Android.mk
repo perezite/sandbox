8643863b8ddcce2d0e9ab97369e2e701ec2778cb
Simon Schwegler <simulo2000@gmail.com>
2016-10-24 12:22:16 +0200

    Removed engine dependency

--- SDL2AndroidDecoupled/build/android/ndk/jni/src/Android.mk
+++ SDL2AndroidDecoupled/build/android/ndk/jni/src/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 # External module
 ##################
-include $(CLEAR_VARS)
-LOCAL_MODULE := openGLSharedLibrary
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/OpenGLEngine/$(TARGET_ARCH_ABI)/libOpenGLEngine.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/OpenGLEngine
-include $(PREBUILT_SHARED_LIBRARY)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := openGLSharedLibrary
+# LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../lib/OpenGLEngine/$(TARGET_ARCH_ABI)/libOpenGLEngine.so
+# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include/OpenGLEngine
+# include $(PREBUILT_SHARED_LIBRARY)
 
 # Application Module
 #####################
@@ -19,7 +19,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
     $(MY_CODE_PATH)/main.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2 openGLSharedLibrary
+LOCAL_SHARED_LIBRARIES := SDL2
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
--- SDL2AndroidDecoupled/src/main.cpp
+++ SDL2AndroidDecoupled/src/main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 
 #include "SDL.h"
-#include "engine.h"
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
@@ -26,174 +25,14 @@ int32_t shaderProgram;
 float width;
 float height;
 
-GLuint LoadShader(const char *shaderSrc, GLenum type)
-{
-	GLuint shader;
-	GLint compiled;
-
-	// Create the shader object
-	shader = glCreateShader(type);
-	if(shader != 0)
-	{
-		// Load the shader source
-		glShaderSource(shader, 1, &shaderSrc, NULL);
-
-		// Compile the shader
-		glCompileShader(shader);
-		// Check the compile status
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-
-		if(!compiled)
-		{
-			GLint infoLen = 0;
-			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-
-			if(infoLen > 1)
-			{
-				char* infoLog = new char[infoLen];
-				glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
-				LOGW("Error compiling shader:\n%s\n", infoLog);
-				delete[] infoLog;
-			}
-			glDeleteShader(shader);
-			shader = 0;
-		}
-	}
-	return shader;
-}
-
-static int init() {
-	// Initialize GL state.
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
-
-	char vShaderStr[] =
-			"attribute vec4 a_vPosition;   \n"
-			"attribute vec4 a_vColor;	   \n"
-			"varying vec4 v_vColor;		   \n"
-			"void main()                   \n"
-			"{                             \n"
-			"   gl_Position = a_vPosition; \n"
-			"	v_vColor = a_vColor;       \n"
-			"}                             \n";
-
-	char fShaderStr[] =
-			"precision mediump float;                   \n"
-			"varying vec4 v_vColor;		 				\n"
-			"void main()                                \n"
-			"{                                          \n"
-			"  gl_FragColor = v_vColor;					\n"
-			"}                                          \n";
-
-	GLuint vertexShader;
-	GLuint fragmentShader;
-	GLint linked;
-
-	// Load the vertex/fragment shaders
-	vertexShader = LoadShader(vShaderStr, GL_VERTEX_SHADER);
-	fragmentShader = LoadShader(fShaderStr, GL_FRAGMENT_SHADER);
-
-	// Create the program object
-	shaderProgram = glCreateProgram();
-	if(shaderProgram == 0)
-	{
-		return -1;
-	}
-
-	// Attach shaders to program
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-
-	// Bind a_vPosition to attribute 0 and a_vColor to 1
-	glBindAttribLocation(shaderProgram, POSITION_PARAMETER_INDEX, "a_vPosition");
-	glBindAttribLocation(shaderProgram, COLOR_PARAMETER_INDEX, "a_vColor");
-
-	// Link the program
-	glLinkProgram(shaderProgram);
-
-	// Check the link status
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linked);
-	if(!linked)
-	{
-		GLint infoLen = 0;
-		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLen);
-
-		if(infoLen > 1)
-		{
-			char* infoLog = new char[infoLen];
-			glGetProgramInfoLog(shaderProgram, infoLen, NULL, infoLog);
-			LOGW("Error linking program:\n%s\n", infoLog);
-
-			delete[] infoLog;
-		}
-
-		glDeleteProgram(shaderProgram);
-		return -1;
-	}
-
-	return 0;
-}
-
 static void display()
-{ 
-    static const int32_t PositionNumElements = 3;
-    static const int32_t ColorNumElements = 4;
-    static const int32_t VertexSize = sizeof(GLfloat) * (PositionNumElements + ColorNumElements);
-
-	glViewport(0, 0, width, height);
-    
+{     
 	// Just fill the screen with a color.
-    clearScreen(0.95f, 0.95f, 0.95f, 1.0f); // call from the external opengl engine
-
-	// Use the program object
-	glUseProgram(shaderProgram);
-    
-	glEnableVertexAttribArray(POSITION_PARAMETER_INDEX);
-	glEnableVertexAttribArray(COLOR_PARAMETER_INDEX);
-    
-    // compute time delta in seconds
-    static Uint32 previousTicks = SDL_GetTicks();
-    Uint32 currentTicks = SDL_GetTicks();
-    Uint32 elapsedTicks = currentTicks - previousTicks;  
-    previousTicks = SDL_GetTicks();
-    float deltaTime = (float)elapsedTicks / 1000.0f;
-    
-    // set untransformed points
-    const float z = 0.0f; 
-	float leftX = -0.3f;
-    float leftY = -0.3f;
-    float rightX = 0.3f;
-    float rightY = -0.3f;
-    float topX = 0.0f;
-    float topY = 0.3f;
-              
-    // compute rotated points
-    // float degToRad = (2.0f * M_PI) / 360.0f; 
-    float omega = M_PI; 
-    static float alpha = 0.0f;
-    alpha += omega * deltaTime;
-    float rotLeftX = cos(alpha)*leftX - sin(alpha)*leftY;
-    float rotLeftY = sin(alpha)*leftX + cos(alpha)*leftY;
-    float rotRightX = cos(alpha)*rightX - sin(alpha)*rightY;
-    float rotRightY = sin(alpha)*rightX + cos(alpha)*rightY;
-    float rotTopX = cos(alpha)*topX - sin(alpha)*topY;
-    float rotTopY = sin(alpha)*topX + cos(alpha)*topY;
-  
-    // render
-	const float color[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-	GLfloat triangle[] = { rotTopX, rotTopY, z,
-        color[0], color[1], color[2], color[3],
-        rotLeftX, rotLeftY, z,
-        color[0], color[1], color[2], color[3],
-        rotRightX, rotRightY, z,
-        color[0], color[1], color[2], color[3] };
-
-	glVertexAttribPointer(POSITION_PARAMETER_INDEX, PositionNumElements, GL_FLOAT, GL_FALSE, VertexSize, triangle);
-	glVertexAttribPointer(COLOR_PARAMETER_INDEX, ColorNumElements, GL_FLOAT, GL_FALSE, VertexSize, &triangle[3]);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-    
-	glDisableVertexAttribArray(POSITION_PARAMETER_INDEX);
-	glDisableVertexAttribArray(COLOR_PARAMETER_INDEX);
+    float rr = (float)(rand()%255)/255.0f;
+    float rg = (float)(rand()%255)/255.0f;
+    float rb = (float)(rand()%255)/255.0f;
+    glClearColor(rr, rg, rb, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 
@@ -208,9 +47,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
     
-    string s = "This is a std::string";
-    SDL_Log("%s\n", s.c_str());
-
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
@@ -237,8 +73,6 @@ int main(int argc, char *argv[])
 
 	gl = SDL_GL_CreateContext(window);
     
-    init();
-    
 	Uint8 done = 0;
 	SDL_Event event;
 	int count = 0;
@@ -257,7 +91,6 @@ int main(int argc, char *argv[])
         display();
              
 		SDL_GL_SwapWindow(window);
-		// SDL_Delay(10);
 	}
 
 	exit(0);
