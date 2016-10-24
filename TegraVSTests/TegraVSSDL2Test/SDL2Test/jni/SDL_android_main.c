f1f0c789dccd007236286310d2dffbcedacad36c
Simon Schwegler <simulo2000@gmail.com>
2016-10-24 17:33:15 +0200

    Implementing SDL2 Tegra Test (WIP)

--- TegraVSTests/TegraVSSDL2Test/SDL2Test/jni/SDL_android_main.c
+++ TegraVSTests/TegraVSSDL2Test/SDL2Test/jni/SDL_android_main.c
@@ -1,5 +1,5 @@
 /*
-    SDL_android_main.c, placed in the public domain by Sam Lantinga  3/13/14
+SDL_android_main.c, placed in the public domain by Sam Lantinga  3/13/14
 */
 #include "SDL2/SDL_internal.h"
 
@@ -9,7 +9,7 @@
 #include "SDL2/SDL_main.h"
 
 /*******************************************************************************
-                 Functions called by JNI
+Functions called by JNI
 *******************************************************************************/
 #include <jni.h>
 
@@ -17,81 +17,63 @@
 extern void SDL_Android_Init(JNIEnv* env, jclass cls);
 
 /* Start up the SDL app */
-JNIEXPORT jstring JNICALL
-Java_com_example_hellojni_HelloJni_stringFromJNI(JNIEnv* env, jclass cls, jobject array)
+JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array)
 {
-    int i;
-    int argc;
-    int status;
+	int i;
+	int argc;
+	int status;
 
-    /* This interface could expand with ABI negotiation, callbacks, etc. */
-    SDL_Android_Init(env, cls);
+	/* This interface could expand with ABI negotiation, callbacks, etc. */
+	SDL_Android_Init(env, cls);
 
-    SDL_SetMainReady();
+	SDL_SetMainReady();
 
-    /* Prepare the arguments. */
+	/* Prepare the arguments. */
 
-    int len = (*env)->GetArrayLength(env, array);
-    // char* argv[1 + len + 1];
-    char* argv = malloc((1 + len + 1) * sizeof(char));
-    argc = 0;
-    /* Use the name "app_process" so PHYSFS_platformCalcBaseDir() works.
-       https://bitbucket.org/MartinFelis/love-android-sdl2/issue/23/release-build-crash-on-start
-     */
-    argv[argc++] = SDL_strdup("app_process");
-    for (i = 0; i < len; ++i) {
-        const char* utf;
-        char* arg = NULL;
-        jstring string = (*env)->GetObjectArrayElement(env, array, i);
-        if (string) {
-            utf = (*env)->GetStringUTFChars(env, string, 0);
-            if (utf) {
-                arg = SDL_strdup(utf);
-                (*env)->ReleaseStringUTFChars(env, string, utf);
-            }
-            (*env)->DeleteLocalRef(env, string);
-        }
-        if (!arg) {
-            arg = SDL_strdup("");
-        }
-        argv[argc++] = arg;
-    }
-    argv[argc] = NULL;
+	int len = (*env)->GetArrayLength(env, array);
+	char* argv = malloc(sizeof(char*) * (1 + len + 1));
+	// char* argv[1 + len + 1];
+	argc = 0;
+	/* Use the name "app_process" so PHYSFS_platformCalcBaseDir() works.
+	https://bitbucket.org/MartinFelis/love-android-sdl2/issue/23/release-build-crash-on-start
+	*/
+	argv[argc++] = SDL_strdup("app_process");
+	for (i = 0; i < len; ++i) {
+		const char* utf;
+		char* arg = NULL;
+		jstring string = (*env)->GetObjectArrayElement(env, array, i);
+		if (string) {
+			utf = (*env)->GetStringUTFChars(env, string, 0);
+			if (utf) {
+				arg = SDL_strdup(utf);
+				(*env)->ReleaseStringUTFChars(env, string, utf);
+			}
+			(*env)->DeleteLocalRef(env, string);
+		}
+		if (!arg) {
+			arg = SDL_strdup("");
+		}
+		argv[argc++] = arg;
+	}
+	argv[argc] = NULL;
 
-    /* Run the application. */
 
-    status = SDL_main(argc, argv);
+	/* Run the application. */
 
-    /* Release the arguments. */
+	status = SDL_main(argc, argv);
 
-    for (i = 0; i < argc; ++i) {
-        SDL_free(argv[i]);
-    }
+	/* Release the arguments. */
 
-    /* Do not issue an exit or the whole application will terminate instead of just the SDL thread */
-    /* exit(status); */
+	for (i = 0; i < argc; ++i) {
+		SDL_free(argv[i]);
+	}
+
+	/* Do not issue an exit or the whole application will terminate instead of just the SDL thread */
+	/* exit(status); */
+
+	return status;
 }
 
 #endif /* __ANDROID__ */
 
 /* vi: set ts=4 sw=4 expandtab: */
-
-/*#include <stdio.h>
-#include <string.h>
-#include <jni.h>
-#include <string>
-
-#include "SharedLibrary/SharedLibrary.h"
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env, jobject thiz )
-{
-SharedLibrary sharedLibrary;
-std::string stringFromSharedLibrary(sharedLibrary.GetPlatformABI());
-jstring result = env->NewStringUTF(stringFromSharedLibrary.c_str());
-return result;
-}
-*/
-
-
--- TegraVSTests/TegraVSSDL2Test/SDL2Test/jni/SDL_main2.cpp
+++ TegraVSTests/TegraVSSDL2Test/SDL2Test/jni/SDL_main2.cpp
@@ -33,7 +33,7 @@ GLuint LoadShader(const char *shaderSrc, GLenum type)
 
 	// Create the shader object
 	shader = glCreateShader(type);
-	if (shader != 0)
+	if(shader != 0)
 	{
 		// Load the shader source
 		glShaderSource(shader, 1, &shaderSrc, NULL);
@@ -43,12 +43,12 @@ GLuint LoadShader(const char *shaderSrc, GLenum type)
 		// Check the compile status
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
 
-		if (!compiled)
+		if(!compiled)
 		{
 			GLint infoLen = 0;
 			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
 
-			if (infoLen > 1)
+			if(infoLen > 1)
 			{
 				char* infoLog = new char[infoLen];
 				glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
@@ -68,22 +68,22 @@ static int init() {
 	glDisable(GL_DEPTH_TEST);
 
 	char vShaderStr[] =
-		"attribute vec4 a_vPosition;   \n"
-		"attribute vec4 a_vColor;	   \n"
-		"varying vec4 v_vColor;		   \n"
-		"void main()                   \n"
-		"{                             \n"
-		"   gl_Position = a_vPosition; \n"
-		"	v_vColor = a_vColor;       \n"
-		"}                             \n";
+			"attribute vec4 a_vPosition;   \n"
+			"attribute vec4 a_vColor;	   \n"
+			"varying vec4 v_vColor;		   \n"
+			"void main()                   \n"
+			"{                             \n"
+			"   gl_Position = a_vPosition; \n"
+			"	v_vColor = a_vColor;       \n"
+			"}                             \n";
 
 	char fShaderStr[] =
-		"precision mediump float;                   \n"
-		"varying vec4 v_vColor;		 				\n"
-		"void main()                                \n"
-		"{                                          \n"
-		"  gl_FragColor = v_vColor;					\n"
-		"}                                          \n";
+			"precision mediump float;                   \n"
+			"varying vec4 v_vColor;		 				\n"
+			"void main()                                \n"
+			"{                                          \n"
+			"  gl_FragColor = v_vColor;					\n"
+			"}                                          \n";
 
 	GLuint vertexShader;
 	GLuint fragmentShader;
@@ -95,7 +95,7 @@ static int init() {
 
 	// Create the program object
 	shaderProgram = glCreateProgram();
-	if (shaderProgram == 0)
+	if(shaderProgram == 0)
 	{
 		return -1;
 	}
@@ -113,12 +113,12 @@ static int init() {
 
 	// Check the link status
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linked);
-	if (!linked)
+	if(!linked)
 	{
 		GLint infoLen = 0;
 		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLen);
 
-		if (infoLen > 1)
+		if(infoLen > 1)
 		{
 			char* infoLog = new char[infoLen];
 			glGetProgramInfoLog(shaderProgram, infoLen, NULL, infoLog);
@@ -135,70 +135,70 @@ static int init() {
 }
 
 static void display()
-{
-	static const int32_t PositionNumElements = 3;
-	static const int32_t ColorNumElements = 4;
-	static const int32_t VertexSize = sizeof(GLfloat) * (PositionNumElements + ColorNumElements);
+{ 
+    static const int32_t PositionNumElements = 3;
+    static const int32_t ColorNumElements = 4;
+    static const int32_t VertexSize = sizeof(GLfloat) * (PositionNumElements + ColorNumElements);
 
 	glViewport(0, 0, width, height);
-
+    
 	// Just fill the screen with a color.
-	clearScreen(0.95f, 0.95f, 0.95f, 1.0f); // call from the external opengl engine
+    clearScreen(0.95f, 0.95f, 0.95f, 1.0f); // call from the external opengl engine
 
-											// Use the program object
+	// Use the program object
 	glUseProgram(shaderProgram);
-
+    
 	glEnableVertexAttribArray(POSITION_PARAMETER_INDEX);
 	glEnableVertexAttribArray(COLOR_PARAMETER_INDEX);
-
-	// compute time delta in seconds
-	static Uint32 previousTicks = SDL_GetTicks();
-	Uint32 currentTicks = SDL_GetTicks();
-	Uint32 elapsedTicks = currentTicks - previousTicks;
-	previousTicks = SDL_GetTicks();
-	float deltaTime = (float)elapsedTicks / 1000.0f;
-
-	// set untransformed points
-	const float z = 0.0f;
+    
+    // compute time delta in seconds
+    static Uint32 previousTicks = SDL_GetTicks();
+    Uint32 currentTicks = SDL_GetTicks();
+    Uint32 elapsedTicks = currentTicks - previousTicks;  
+    previousTicks = SDL_GetTicks();
+    float deltaTime = (float)elapsedTicks / 1000.0f;
+    
+    // set untransformed points
+    const float z = 0.0f; 
 	float leftX = -0.3f;
-	float leftY = -0.3f;
-	float rightX = 0.3f;
-	float rightY = -0.3f;
-	float topX = 0.0f;
-	float topY = 0.3f;
-
-	// compute rotated points
-	// float degToRad = (2.0f * M_PI) / 360.0f; 
-	float omega = M_PI;
-	static float alpha = 0.0f;
-	alpha += omega * deltaTime;
-	float rotLeftX = cos(alpha)*leftX - sin(alpha)*leftY;
-	float rotLeftY = sin(alpha)*leftX + cos(alpha)*leftY;
-	float rotRightX = cos(alpha)*rightX - sin(alpha)*rightY;
-	float rotRightY = sin(alpha)*rightX + cos(alpha)*rightY;
-	float rotTopX = cos(alpha)*topX - sin(alpha)*topY;
-	float rotTopY = sin(alpha)*topX + cos(alpha)*topY;
-
-	// render
+    float leftY = -0.3f;
+    float rightX = 0.3f;
+    float rightY = -0.3f;
+    float topX = 0.0f;
+    float topY = 0.3f;
+              
+    // compute rotated points
+    // float degToRad = (2.0f * M_PI) / 360.0f; 
+    float omega = M_PI; 
+    static float alpha = 0.0f;
+    alpha += omega * deltaTime;
+    float rotLeftX = cos(alpha)*leftX - sin(alpha)*leftY;
+    float rotLeftY = sin(alpha)*leftX + cos(alpha)*leftY;
+    float rotRightX = cos(alpha)*rightX - sin(alpha)*rightY;
+    float rotRightY = sin(alpha)*rightX + cos(alpha)*rightY;
+    float rotTopX = cos(alpha)*topX - sin(alpha)*topY;
+    float rotTopY = sin(alpha)*topX + cos(alpha)*topY;
+  
+    // render
 	const float color[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 	GLfloat triangle[] = { rotTopX, rotTopY, z,
-		color[0], color[1], color[2], color[3],
-		rotLeftX, rotLeftY, z,
-		color[0], color[1], color[2], color[3],
-		rotRightX, rotRightY, z,
-		color[0], color[1], color[2], color[3] };
+        color[0], color[1], color[2], color[3],
+        rotLeftX, rotLeftY, z,
+        color[0], color[1], color[2], color[3],
+        rotRightX, rotRightY, z,
+        color[0], color[1], color[2], color[3] };
 
 	glVertexAttribPointer(POSITION_PARAMETER_INDEX, PositionNumElements, GL_FLOAT, GL_FALSE, VertexSize, triangle);
 	glVertexAttribPointer(COLOR_PARAMETER_INDEX, ColorNumElements, GL_FLOAT, GL_FALSE, VertexSize, &triangle[3]);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-
+    
 	glDisableVertexAttribArray(POSITION_PARAMETER_INDEX);
 	glDisableVertexAttribArray(COLOR_PARAMETER_INDEX);
 }
 
 
-int SDL_main(int argc, char *argv[])
-{
+int main(int argc, char *argv[])
+{    
 	SDL_Window* window = 0;
 	SDL_GLContext gl = 0;
 
@@ -207,19 +207,20 @@ int SDL_main(int argc, char *argv[])
 		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
 		return 1;
 	}
-
-	string s = "This is a std::string";
+    
+    string s = "This is a std::string";
+    SDL_Log("%s\n", s.c_str());
 
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
+   
 	SDL_DisplayMode mode;
 	SDL_GetDisplayMode(0, 0, &mode);
 	width = mode.w;
@@ -229,20 +230,21 @@ int SDL_main(int argc, char *argv[])
 
 	if (window == 0)
 	{
+		SDL_Log("Failed to create window.");
 		SDL_Quit();
 		return 1;
 	}
 
 	gl = SDL_GL_CreateContext(window);
-
-	init();
-
+    
+    init();
+    
 	Uint8 done = 0;
 	SDL_Event event;
 	int count = 0;
-	while (!done)
+	while(!done)
 	{
-		while (SDL_PollEvent(&event))
+		while(SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN || event.type == SDL_FINGERDOWN)
 			{
@@ -250,9 +252,12 @@ int SDL_main(int argc, char *argv[])
 			}
 		}
 
-		display();
+		SDL_Log("%d\n", count++);
 
+        display();
+             
 		SDL_GL_SwapWindow(window);
+		// SDL_Delay(10);
 	}
 
 	exit(0);
