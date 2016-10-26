daab64ab54934624756c1be37ca0bc685e685bc4
Simon Schwegler <simulo2000@gmail.com>
2016-10-26 19:00:43 +0200

    Implementing VS-Tegra test for imported Makefile-based project with SDL2 (WIP)

---
+++ TegraVSTests/TegraVSSDL2WithMakefilesTest/build/android/ndk/gen/R.java.d
@@ -0,0 +1,3 @@
+Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\gen\com\example\hellojni\R.java \
+ : Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\res\values\strings.xml \
+Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\bin\AndroidManifest.xml \
