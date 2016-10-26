b905356149bb6a170f10b7dfd550a65026327d4b
Simon Schwegler <simulo2000@gmail.com>
2016-10-26 19:02:42 +0200

    Cleanup and gitignore

--- TegraVSTests/TegraVSSDL2WithMakefilesTest/build/android/ndk/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\gen\com\example\hellojni\R.java \
- : Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\res\values\strings.xml \
-Z:\Indie\Development\Simulo\sandbox\TegraVSTests\TegraVSSDL2WithMakefilesTest\build\android\ndk\bin\AndroidManifest.xml \
