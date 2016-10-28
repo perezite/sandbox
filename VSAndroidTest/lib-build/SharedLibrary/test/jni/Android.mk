6a99842c39fa8148ee92524a19eacb0b4221677e
Simon Schwegler <simulo2000@gmail.com>
2016-10-28 12:36:00 +0200

    Adding test project for shared library (WIP)

---
+++ VSAndroidTest/lib-build/SharedLibrary/test/jni/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+# Shared Library module
+#######################
+include $(CLEAR_VARS)
+LOCAL_MODULE := SharedLibrary
+LOCAL_SRC_FILES := ../../libs/$(TARGET_ARCH_ABI)/libSharedLibrary.so
+LOCAL_EXPORT_C_INCLUDES := ../../jni
+include $(PREBUILT_SHARED_LIBRARY)
+
+# Main module
+#################
+include $(CLEAR_VARS)
+LOCAL_MODULE    := hello-jni
+LOCAL_SRC_FILES := hello-jni.cpp
+LOCAL_SHARED_LIBRARIES := SharedLibrary
+include $(BUILD_SHARED_LIBRARY)
