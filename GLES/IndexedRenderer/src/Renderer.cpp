09cc5e05b755f08598fdbc48906bb2c03fa00cd2
Simon Schwegler <simulo2000@gmail.com>
2018-11-30 23:51:49 +0100

    Implementing index renderer (wip)

--- GLES/IndexedRenderer/src/Renderer.cpp
+++ GLES/IndexedRenderer/src/Renderer.cpp
@@ -8,111 +8,32 @@
 namespace sb
 {
 	void Renderer::init()
+	{ }
+
+	void Renderer::add(Drawable* drawable)
 	{
-		
+		m_indexList.add(drawable);
 	}
 
 	void Renderer::remove(Drawable* drawable)
 	{
-		if (m_drawableInfos.find(drawable) == m_drawableInfos.end() && 
-			std::find(m_drawablesToAdd.begin(), m_drawablesToAdd.end(), (drawable)) == m_drawables.end())
-			Error().die() << "Trying to remove a non-existing drawable, aborting";
-
-		m_drawablesToRemove.push_back(drawable);
-		m_numIndicesToAdd -= drawable->mesh.getIndexCount();
-		m_firstAffectedDrawable = std::min(m_firstAffectedDrawable, m_drawableInfos[drawable].positionInDrawableList);
-	}
-
-	void Renderer::add(Drawable* drawable)
-	{
-		if (m_drawableInfos.find(drawable) != m_drawableInfos.end() && 
-			std::find(m_drawablesToRemove.begin(), m_drawablesToRemove.end(), drawable) == m_drawables.end())
-			Error().die() << "Trying to add an already existing drawable, aborting";
-
-		m_drawablesToAdd.push_back(drawable);
-		m_numIndicesToAdd += drawable->mesh.getIndexCount();
+		m_indexList.remove(drawable);
 	}
 
 	void Renderer::render()
 	{
-		addDrawables();
-		removeDrawables();
-		resizeIndices();
-		recalcIndices();
-
+		m_indexList.calculate();
+		m_indexList.print();
 		print();
-		cleanupRender();
  	}
-
-	void Renderer::addDrawables()
-	{
-		if (m_drawablesToAdd.empty()) 
-			return;
-
-		for (std::size_t i = 0; i < m_drawablesToAdd.size(); i++) 
-			m_drawables.push_back(m_drawablesToAdd[i]);
-	}
-
-	void Renderer::removeDrawables() 
-	{
-		std::vector<Drawable*>::iterator it = m_drawables.end();
-		for (std::size_t i = 0; i < m_drawablesToRemove.size(); i++) {
-			it = std::remove(m_drawables.begin(), it, m_drawablesToRemove[i]);
-			m_drawableInfos.erase(m_drawablesToRemove[i]);
-		}
-
-		m_drawables.erase(it, m_drawables.end());
-	}
-
-
-	void Renderer::resizeIndices()
-	{
-		m_indices.resize(m_indices.size() + m_numIndicesToAdd);
-	}
-
-	void Renderer::recalcIndices()
-	{
-		bool allAffected = m_firstAffectedDrawable == 0;
-		Drawable* lastUnaffectedDrawable = allAffected ? NULL : m_drawables[m_firstAffectedDrawable - 1];
-		DrawableInfo* lastUnaffectedInfo = allAffected ? NULL : &m_drawableInfos[lastUnaffectedDrawable];
-		std::size_t positionInDrawableList = allAffected ? 0 : m_firstAffectedDrawable;
-		std::size_t positionInIndexList = allAffected ? 0 : lastUnaffectedInfo->positionInIndexList + lastUnaffectedDrawable->mesh.getIndexCount();
-		GLushort offsetInIndexList = allAffected ? 0 : lastUnaffectedInfo->offsetInIndexList + (GLushort)lastUnaffectedDrawable->mesh.getVertexCount();
-
-		for (std::size_t i = m_firstAffectedDrawable; i < m_drawables.size(); i++) {
-			const std::vector<GLushort>& indices = m_drawables[i]->mesh.getIndices();
-			for (std::size_t j = 0; j < indices.size(); j++)
-				m_indices[positionInIndexList + j] = offsetInIndexList + indices[j];
-
-			Drawable* drawable = m_drawables[i];
-			m_drawableInfos[drawable] = DrawableInfo(positionInDrawableList, positionInIndexList, offsetInIndexList);
-			positionInDrawableList += 1;
-			positionInIndexList += drawable->mesh.getIndexCount();
-			offsetInIndexList += (GLushort)drawable->mesh.getVertexCount();
-		}
-	}
 
 	void Renderer::print()
 	{
-		std::cout << "number of drawables: " << m_drawables.size() << std::endl;
-		std::cout << "indices:" << std::endl;
-		for (std::size_t i = 0; i < m_drawables.size(); i++) {
-			std::size_t startPos = m_drawableInfos[m_drawables[i]].positionInIndexList;
-			std::size_t numIndices = m_drawables[i]->mesh.getIndexCount();
-			for (std::size_t j = 0; j < numIndices; j++)
-				std::cout << m_indices[startPos + j] << " ";
+		const::std::vector<GLushort>& indices =  m_indexList.getIndices();
 
-			std::cout << "| ";
-		}
-
+		std::cout << "Raw indices: ";
+		for (std::size_t i = 0; i < indices.size(); i++)
+			std::cout << indices[i] << " ";
 		std::cout << std::endl;
-	}
-
-	void Renderer::cleanupRender()
-	{
-		m_drawablesToAdd.clear();
-		m_numIndicesToAdd = 0;
-		m_drawablesToRemove.clear();
-		m_firstAffectedDrawable = m_drawables.size();
 	}
 }
--- GLES/IndexedRenderer/src/Renderer.h
+++ GLES/IndexedRenderer/src/Renderer.h
@@ -4,63 +4,25 @@
 #include "Drawable.h"
 #include "Vertex.h"
 #include "Shader.h"
-#include <vector>
-#include <list>
+#include "IndexList.h" 
 
 namespace sb
 {
-	struct DrawableInfo {
-		DrawableInfo(std::size_t positionInDrawableList_ = 0, std::size_t positionInIndexList_ = 0, GLushort offsetInIndexList_ = 0)
-			: positionInDrawableList(positionInDrawableList_), positionInIndexList(positionInIndexList_), offsetInIndexList(offsetInIndexList_)
-		{ }
-		
-		std::size_t positionInDrawableList;
-		std::size_t positionInIndexList;
-		GLushort offsetInIndexList;
-	};
-
 	class Renderer
 	{
 	public:
-		Renderer()
-		{
-			cleanupRender();
-		}
-
 		void init();
-
-		void remove(Drawable* drawable);
 
 		void add(Drawable* drawable);
 
+		void remove(Drawable* drawable);
+
 		void render();
 
-	protected:
-		void addDrawables();
-
-		void removeDrawables();
-
-		void resizeIndices();
-
-		void recalcIndices();
-
+	protected: 
 		void print();
 
-		void cleanupRender();
-
 	private:
-		std::vector<Drawable*> m_drawables;
-
-		std::map<Drawable*, DrawableInfo> m_drawableInfos;
-
-		std::vector<Drawable*> m_drawablesToAdd;
-
-		std::vector<Drawable*> m_drawablesToRemove;
-
-		int m_numIndicesToAdd;
-
-		std::vector<GLushort> m_indices;
-
-		std::size_t m_firstAffectedDrawable;
+		IndexList m_indexList;
 	};
 }
