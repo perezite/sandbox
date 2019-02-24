0c3857385c648066f4da3ab8b38f93a6174eec8f
Simon Schwegler <simulo2000@gmail.com>
2019-02-24 16:39:44 +0100

    Finished implementing basic renderer demo

--- Renderer/SimpleRenderer/src/Main.cpp
+++ Renderer/SimpleRenderer/src/Main.cpp
@@ -5,6 +5,33 @@
 #include <SDL2/SDL.h>
 #include <vector>
 
+void demo4()
+{
+	sb::Window window;
+
+	std::vector<sb::Vertex> drawables = {
+		// triangle
+		sb::Vertex(sb::Vector2f(   -1, -1), sb::Color(1, 0, 0, 1)),
+		sb::Vertex(sb::Vector2f(    0, -1), sb::Color(0, 1, 0, 1)),
+		sb::Vertex(sb::Vector2f(-0.5f,  0), sb::Color(0, 0, 1, 1)),
+
+		// rectangle
+		sb::Vertex(sb::Vector2f(    0,     0),	sb::Color(1, 0, 0, 1)),
+		sb::Vertex(sb::Vector2f(	1,	   0),	sb::Color(0, 1, 0, 1)),
+		sb::Vertex(sb::Vector2f(	0,	   1),	sb::Color(0, 0, 1, 1)),
+		sb::Vertex(sb::Vector2f(	1,	   0),	sb::Color(0, 1, 0, 1)),
+		sb::Vertex(sb::Vector2f(	1,	   1),	sb::Color(0, 1, 1, 1)),
+		sb::Vertex(sb::Vector2f(	0,	   1),	sb::Color(0, 0, 1, 1))
+	};;
+
+	while (window.isOpen()) {
+		window.update();
+		window.clear();
+		window.draw(drawables);
+		window.display();
+	}
+}
+
 void demo3()
 {
 	sb::Window window;
@@ -57,7 +84,9 @@ int main(int argc, char* args[])
 {
 	SDL_Log("Simple Renderer: Build %s %s", __DATE__, __TIME__);
 
-	demo3();
+	demo4();
+
+	// demo3();
 
 	// demo2();
 
