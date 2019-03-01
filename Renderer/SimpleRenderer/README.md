ae2df622d6924db21b160a9f96515b6678bef54e
Simon Schwegler <simulo2000@gmail.com>
2019-03-01 13:22:44 +0100

    Cleanup

--- Renderer/SimpleRenderer/README.md
+++
@@ -1,3 +0,0 @@
-# Content
-The most simple cross-compatible gl/gles renderer:
- * Render a triangle and a quad
--- Renderer/SimpleRenderer/src/GL.h
+++
@@ -1,8 +0,0 @@
-#ifdef  WIN32
-	#include <gl/glew.h>
-	#include <SDL2/SDL_opengl.h>
-	#include <gl/glu.h>
-#else
-	#include <GLES2/gl2.h>	
-	#include <GLES2/gl2ext.h>
-#endif 
