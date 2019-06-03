5fc27b06061bcd343e401acd43687c7461c3e62e
Prez <10709507+perezite@users.noreply.github.com>
2019-06-03 16:49:05 +0200

    Math fixing wip

--- Renderer/15_ParticleSystems/src/Easing.h
+++ Renderer/15_ParticleSystems/src/Easing.h
@@ -80,20 +80,21 @@ namespace sb
 		}
 
 		static inline float computeBounceOut(float t, float b, float c, float d) {
-			if ((t /= d) < (1 / 2.75f)) {
-				return c * (7.5625f * t * t) + b;
+			float t0 = t / d;
+			if (t0 < (1 / 2.75f)) {
+				return c * (7.5625f * t0 * t0) + b;
 			}
-			else if (t < 2 / 2.75f) {
-				float postFix = t -= (1.5f / 2.75f);
-				return c * (7.5625f * postFix * t + .75f) + b;
+			else if (t0 < 2 / 2.75f) {
+				float postFix = t0 -= (1.5f / 2.75f);
+				return c * (7.5625f * postFix * t0 + .75f) + b;
 			}
-			else if (t < 2.5 / 2.75) {
-				float postFix = t -= (2.25f / 2.75f);
-				return c * (7.5625f * postFix * t + .9375f) + b;
+			else if (t0 < 2.5 / 2.75) {
+				float postFix = t0 -= (2.25f / 2.75f);
+				return c * (7.5625f * postFix * t0 + .9375f) + b;
 			}
 			else {
-				float postFix = t -= (2.625f / 2.75f);
-				return c * (7.5625f * postFix * t + .984375f) + b;
+				float postFix = t0 -= (2.625f / 2.75f);
+				return c * (7.5625f * postFix * t0 + .984375f) + b;
 			}
 		}
 
