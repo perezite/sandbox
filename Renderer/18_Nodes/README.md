4aa545f10531e0e610c337100f68e1c3d9645091
simulo <simulo2000@gmail.com>
2020-07-10 23:08:41 +0200

    Removed outdated entity component stuff

--- Renderer/18_Nodes/README.md
+++
@@ -1,3 +0,0 @@
-# Content
-Instead of scenes, entities and components, we have scenes and nodes.
-A scene can contain a graph of nodes. A node can be updated and drawn and can contain child nodes.
--- Renderer/18_Nodes/src/LayerType.cpp
+++
@@ -1,7 +0,0 @@
-#include "LayerType.h"
-
-namespace sb {
-	const bool operator<(const LayerType& left, const LayerType& right) {
-		return std::tie(left.drawStates, left.primitiveType) < std::tie(right.drawStates, right.primitiveType);
-	}
-}
