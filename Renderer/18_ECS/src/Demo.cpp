058c8da7fc4787691dcbe5c6fe0e057289437e39
simulo <simulo2000@gmail.com>
2020-07-10 23:06:01 +0200

    Implementing ecs

--- Renderer/18_ECS/src/Demo.cpp
+++ Renderer/18_ECS/src/Demo.cpp
@@ -2,7 +2,10 @@
 #include "Window.h"
 #include "Input.h"
 #include "Stopwatch.h"
+#include "Sprite.h"
 #include <iostream>
+
+using namespace sb;
 
 namespace demo
 {
@@ -20,15 +23,39 @@ namespace demo
 		return delta;
 	}
 
-	void run()
-	{
+	class Sprite0 {
+		Sprite _sprite;
+
+	public:
+		Sprite0(sb::Texture& texture) {
+			_sprite = Sprite(texture);
+		}
+
+		void setScale(float x, float y) { _sprite.setScale(x, y); }
+
+		void setScale(float s) { setScale(s, s); }
+
+		virtual void draw(DrawTarget& window, DrawStates states = DrawStates::getDefault()) { _sprite.draw(window, states); }
+	};
+
+	void demo0() {
 		sb::Window window;
+		sb::Texture texture("Textures/YellowBlock.png");
+		Sprite0 sprite(texture);
+		sprite.setScale(.3f);
 
 		while (window.isOpen()) {
 			sb::Input::update();
 			window.update();
+
 			window.clear(sb::Color(1, 1, 1, 1));
+			sprite.draw(window);
 			window.display();
 		}
+	}
+
+	void run()
+	{
+		demo0();
 	}
 }
